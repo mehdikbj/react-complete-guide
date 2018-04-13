231cd493c83eb89c6a92f255a962577cd89ff62b
mehdi kabbaj <mka@octo.com>
2018-04-13 14:56:33 +0100

    Understanding the Base Features & Syntax

--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
+
 
 class App extends Component {
+  state = {
+    persons: [
+        { name: 'Mehdi', age: 26},
+        { name: 'Hamza', age: 22}
+    ]
+  };
+
+  switchNameHandler = (newName) => {
+      this.setState({
+          persons: [
+              { name: newName, age: 26},
+              { name: 'MY Hamza', age: 22}
+          ]
+      })
+  };
+
+  nameChangedHandler = (event) => {
+      this.setState({
+          persons: [
+              { name: 'Mehdi', age: 26},
+              { name: event.target.value, age: 22}
+          ]
+      })
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, i'm a react app</h1>
+        <button
+            style={style}
+            onClick={this.switchNameHandler.bind(this, 'MEHDI KKKK')}>Switch Name</button>
+        <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'MEHDI !!!')}
+        > and i'm feeling gooooood !!!! </Person>
+        <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            changed={this.nameChangedHandler}
+        />
       </div>
     );
   }
