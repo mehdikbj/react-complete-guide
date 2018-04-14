205f7866400b42de63f6241302cf084967ce6107
mehdi kabbaj <mka@octo.com>
2018-04-14 11:25:38 +0100

    Styling React Components & Elements 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import Classes from './App.css';
 import Person from './Person/Person'
 
 
@@ -74,19 +74,19 @@ class App extends Component {
       style.backgroundColor = 'red';
     };
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+        assignedClasses.push(Classes.red);
     }
 
       if (this.state.persons.length <= 1) {
-          classes.push('bold');
+        assignedClasses.push(Classes.bold);
       }
 
     return (
-      <div className="App">
+      <div className={Classes.App}>
         <h1>Hi, i'm a react app</h1>
-        <p className={classes.join(' ')}>This is really working !!!</p>
+        <p className={assignedClasses.join(' ')}>This is really working !!!</p>
         <button
             style={style}
             onClick={this.togglePersonHandler}>Switch Name
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Person.css';
+import Classes from './Person.css';
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <div className={Classes.Person}>
             <p onClick={props.click}>I'm a person : My name is { props.name} and my age is { props.age}</p>
             <p> { props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
